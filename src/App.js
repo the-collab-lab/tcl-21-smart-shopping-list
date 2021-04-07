6c4133c1a616ca5f48dcb7984407acb8a4fc7587
tarynblakemiller <yourfriendtaryn@gmail.com>
2021-04-07 10:47:47 -0400

    added links to list view and add item view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+// import './App.css';
+import List from './components/List/List';
+import AddItem from './components/AddItem/AddItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/list">
+            <List />
+          </Route>
+          <Route path="/additem">
+            <AddItem />
+          </Route>
+        </Switch>
+        <Link to="/list">List</Link>
+        <Link to="/additem">Add Item</Link>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/List/List.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const List = () => {
+  return <div>Current List</div>;
+};
+
+export default List;
