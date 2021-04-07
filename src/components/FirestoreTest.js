e207f8c8eec87849b086cf455ae62dd0112f0f33
CodeNameTam <CodeNameTam@yahoo.com>
2021-04-07 13:34:25 -0400

    Added comments for clarity on functionality

--- src/components/FirestoreTest.js
+++ src/components/FirestoreTest.js
@@ -4,6 +4,7 @@ import { firestore } from '../lib/firebase.js';
 
 function FirestoreTest() {
   return (
+    // Creating component with embedded children
     <section>
       <ShoppingListControl />
     </section>
@@ -11,48 +12,54 @@ function FirestoreTest() {
 }
 
 function ShoppingListControl() {
+  // component housing headquarters
   const listData = firestore.collection('lists');
-  const query = listData.orderBy('createdAt');
-  const [lists] = useCollectionData(query, { idField: 'id' });
-  const [listName, setListName] = useState('');
+  const query = listData.orderBy('createdAt'); // query the subset of documents and order them by date
+  const [lists] = useCollectionData(query, { idField: 'id' }); // listen and re-render documents in real time
+  const [listName, setListName] = useState(''); // starting the form as an empty string
 
   const handleAddingList = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // stopping the auto-refresh of the page
     await listData.add({
+      // writing a new document to firestore with the select values/fields
       name: listName,
       createdAt: Date.now(),
     });
-    setListName('');
+    setListName(''); // reset back to empty screen after submission
   };
 
   return (
     <>
       <h1>Smart Shopping App</h1>
       <main>
+        <h2> My Shopping Lists</h2>
+
+        <ul>
+          {lists && // map over the array of list items
+            lists.map((list) => <ShoppingList key={list.id} list={list} />)}
+        </ul>
+
         <form onSubmit={handleAddingList}>
           <input
             value={listName}
-            onChange={(e) => setListName(e.target.value)}
+            onChange={(e) => setListName(e.target.value)} // recording user input
             placeholder="List Name"
           />
           <button type="submit" disabled={!listName}>
             Add New List
           </button>
         </form>
-        <h2> My Shopping Lists</h2>
-        <ul>
-          {lists &&
-            lists.map((list) => <ShoppingList key={list.id} list={list} />)}
-        </ul>
       </main>
     </>
   );
 
   function ShoppingList(props) {
+    // the child component that renders each list entry with a delete button
     const { name, id } = props.list;
     const docRef = firestore.doc(`lists/${id}`);
 
     const handleDelete = async () => {
+      // delete button functionality with tests
       try {
         await docRef.delete();
         console.log('Document successfully deleted from Firestore!');
