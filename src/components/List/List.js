f95b5248ee9129b3daf132a2dc855514c65a4247
Vanessa Stewart <vstewart1213@gmail.com>
2021-04-26 09:53:35 -0700

    Add prompt to add item when list is empty

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,14 +6,12 @@ import NavBar from '../NavBar/NavBar';
 import { useHistory } from 'react-router-dom';
 
 const List = ({ token, setToken }) => {
-  // Option 1: A&V's Implementation
   const groceryItemsRef = firestore.collection('groceryItems');
   const query = groceryItemsRef.where('token', '==', token);
 
-  // Option 2: Compatible with T&T's Implementation
-  // const query = firestore.collection(token);
-
-  const [groceryItems] = useCollectionData(query, { idField: 'id' });
+  const [groceryItems, loading, error] = useCollectionData(query, {
+    idField: 'id',
+  }); // added constants that detect the query's state so that page doesn't render when the query is still loading
   const history = useHistory();
 
   const clearToken = () => {
@@ -35,13 +33,22 @@ const List = ({ token, setToken }) => {
           <button data-testid="clearTokenButton" onClick={clearToken}>
             Return to welcome screen
           </button>
+
           <div className="grocery-list">
-            <ul>
-              {groceryItems &&
-                groceryItems.map((item) => (
+            {!loading && !error && groceryItems && groceryItems.length ? (
+              <ul>
+                {groceryItems.map((item) => (
                   <GroceryItem key={item.id} item={item} />
                 ))}
-            </ul>
+              </ul>
+            ) : (
+              <>
+                <p>Your shopping list is currently empty.</p>
+                <button onClick={() => history.push('/additem')}>
+                  Add Item
+                </button>
+              </>
+            )}
           </div>
           <NavBar />
         </>
