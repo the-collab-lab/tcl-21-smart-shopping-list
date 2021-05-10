e03b3cbeea3fa306945832241662bcb7c2e4168e
Vanessa Stewart <vstewart1213@gmail.com>
2021-05-10 08:54:00 -0700

    Add aria label and styling to groceryitems

--- src/components/GroceryItem/GroceryItem.js
+++ src/components/GroceryItem/GroceryItem.js
@@ -2,16 +2,37 @@ import React, { useState, useEffect } from 'react';
 import { firestore } from '../../lib/firebase.js';
 import { differenceInDays, addDays } from 'date-fns';
 import calculateEstimate from '../../lib/estimates';
+import './GroceryItem.css';
 
 const GroceryItem = ({ item }) => {
   const groceryItem = firestore.collection('groceryItems').doc(item.id);
   const [isChecked, setChecked] = useState(false);
+  const [ariaLabel, setAriaLabel] = useState('');
+  const [background, setBackground] = useState('');
   const currentDate = Date.now();
   const ONE_DAY_IN_MILLISECONDS = 60 * 60 * 24 * 1000;
 
   useEffect(() => {
     if (item.purchaseDate > Date.now() - ONE_DAY_IN_MILLISECONDS) {
       setChecked(true);
+    }
+
+    if (
+      currentDate - item.purchaseDate >=
+        2 * (item.nextPurchaseDate - item.purchaseDate) ||
+      item.numberOfPurchases === 0
+    ) {
+      setAriaLabel('Inactive');
+      setBackground('background-grey');
+    } else if (item.howSoon < 7) {
+      setAriaLabel('Need to buy soon');
+      setBackground('background-red');
+    } else if (item.howSoon >= 7 && item.howSoon <= 30) {
+      setAriaLabel('Kind of soon');
+      setBackground('background-yellow');
+    } else if (item.howSoon > 30) {
+      setAriaLabel('Need to buy not soon');
+      setBackground('background-green');
     }
   }, []);
 
@@ -36,7 +57,7 @@ const GroceryItem = ({ item }) => {
         purchaseDate: currentDate,
         howSoon: daysUntilNextPurchase,
         numberOfPurchases: item.numberOfPurchases + 1,
-        nextPurchaseDate: addDays(currentDate, daysUntilNextPurchase),
+        nextPurchaseDate: addDays(currentDate, daysUntilNextPurchase).getTime(),
       })
       .then(() => {
         console.log('Document successfully updated!', item.purchaseDate);
@@ -51,9 +72,26 @@ const GroceryItem = ({ item }) => {
     setChecked(false);
   };
 
+  // const getStyles = () => {
+  //   if (item.howSoon < 7) {
+  //       setAriaLabel('Need to buy soon')
+  //       setBackground('background-red')
+  //   } else if (item.howSoon >=7 && item.howSoon <=30) {
+  //       setAriaLabel('Kind of soon')
+  //       setBackground('background-yellow')
+  //   } else if (item.howSoon > 30) {
+  //       setAriaLabel('Need to buy not soon')
+  //       setBackground('background-green')
+  //   } else if ((currentDate - item.purchaseDate) >= 2 * (item.nextPurchaseDate.getTime() - item.purchaseDate)
+  //     || item.numberOfPurchases === 0) {
+  //       setAriaLabel('Inactive')
+  //       setBackground('background-grey')
+  //     }
+  // }
+
   return (
     <>
-      <p>
+      <p aria-label={ariaLabel} className={background}>
         <input
           type="checkbox"
           onChange={() => (isChecked ? resetDate() : setNewDate())}
